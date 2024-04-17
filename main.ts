6d243f604ce3cbf6cd620e110b03c314c0f02b9a
georgempla <92173276+georgempla@users.noreply.github.com>
2024-04-17 12:22:48 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+input.onButtonPressed(Button.A, function () {
+    servos.P0.setAngle(90)
+    spots += -1
+    car_in += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    if (!(car_in == car_out)) {
+        garage_out()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    servos.P0.setAngle(180)
+})
+function garage_out () {
+    servos.P1.setAngle(90)
+    spots += 1
+    car_out += 1
+    basic.pause(5000)
+    basic.pause(5000)
+    servos.P1.setAngle(180)
+}
+let car_out = 0
+let car_in = 0
+servos.P0.setAngle(180)
+servos.P1.setAngle(180)
+let spots = 9
+basic.forever(function () {
+    basic.showString("" + convertToText(car_in) + "-" + convertToText(car_out) + "-" + convertToText(spots))
+})
